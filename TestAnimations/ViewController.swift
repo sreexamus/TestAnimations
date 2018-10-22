d92c99c4d94c65f225b76394712c0382d17fff14
sreekanthreddy.iraga <sreekanthreddy.iragamreddy@schwab.com>
2018-10-22 10:33:03 -0500

    Initial Commit

---
+++ TestAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAnimations
+//
+//  Created by Iragam Reddy, Sreekanth Reddy on 10/22/18.
+//  Copyright © 2018 Iragam Reddy, Sreekanth Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
